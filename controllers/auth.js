4861a319b855881fcaf644eceb9b2447d831e64f
devarshee <ddevarshee12@gmail.com>
2023-02-04 14:57:09 +0530

    commit from devarshee branch login endpoints added

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,13 +1,15 @@
 const Creater = require("../models/Creater");
 
 
+const bcrypt = require("bcryptjs");
+
 
 const register = async(req, res) => {
-
+    
     try{
-        const creater = await Creater.create(req.body);
+        const creaters = await Creater.create(req.body);
 
-        res.json(creater);
+        res.json(creaters);
     }
     catch(err){
         console.log(err);
@@ -19,6 +21,42 @@ const register = async(req, res) => {
 
 
 const login = async(req, res) => {
+    const {type, email, password} = req.body;
+    if(type === "creater"){
+        try{
+            
+            const creater = await Creater.findOne({email: email});
+
+
+            if(!creater){
+                return res.status(404).json({"status" : "email invalid"});
+            }
+            
+            const dbPassword = creater.password;
+            
+            bcrypt.compare(password, dbPassword, (err, data) => {
+                if(err){
+                    return res.status(404).json({"status": "something went wrong"});
+                }
+
+                if(data){
+                    return res.status(200).json({"status": "login successful"});
+                }
+                else{
+                    return res.status(404).json({"status": "invalid password"});
+                }
+            })
+
+        }
+        catch(err){
+            console.log(err);
+            res.json(err);
+        }
+    }
+    if(type === "member"){
+        
+
+    }
 
 }
 
